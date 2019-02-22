ad6e827927ca84c5a75f5180bcc15ecf728bee68
David Muckle <dvdmuckle@dvdmuckle.xyz>
2019-02-21 19:47:46 -0500

    Update API key

--- .deploy.sh
+++ .deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-source ~/virtualenv/python2.7/bin/activate
-pip install copr-cli simplejson jinja2
-openssl aes-256-cbc -K $encrypted_403c23673bf1_key -iv $encrypted_403c23673bf1_iv -in .copr.enc -out .copr -d
-copr-cli --config .copr build --nowait ${COPR_REPOSITORY} *.src.rpm
-sudo apt-get install alien
-sudo alien *[^src].rpm
-gem install package_cloud
-package_cloud yank dvdmuckle/minsh/ubuntu/trusty *.deb
-package_cloud push dvdmuckle/minsh/ubuntu/trusty *.deb
-package_cloud yank dvdmuckle/minsh/ubuntu/xenial *.deb
-package_cloud push dvdmuckle/minsh/ubuntu/xenial *.deb
+	source ~/virtualenv/python2.7/bin/activate
+	pip install copr-cli simplejson jinja2
+	openssl aes-256-cbc -K $encrypted_403c23673bf1_key -iv $encrypted_403c23673bf1_iv -in copr.enc -out .copr -d
+	copr-cli --config .copr build --nowait ${COPR_REPOSITORY} *.src.rpm
+	sudo apt-get install alien
+	sudo alien *[^src].rpm
+	gem install package_cloud
+	package_cloud yank dvdmuckle/minsh/ubuntu/trusty *.deb
+	package_cloud push dvdmuckle/minsh/ubuntu/trusty *.deb
+	package_cloud yank dvdmuckle/minsh/ubuntu/xenial *.deb
+	package_cloud push dvdmuckle/minsh/ubuntu/xenial *.deb
 else
 	echo "Not on master branch, not deploying"
 fi
